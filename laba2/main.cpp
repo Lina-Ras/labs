923332a3c92fa3945ca64fc28ebe7a62da68edb5
Lina Dva <Anhelina.Pashkowets@gmail.com>
2020-09-18 11:44:11 +0300

    add correct main.cpp

---
+++ laba2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, min;
+    cout << "Enter n:" << endl;
+    cin>>n;
+
+    int a[n];
+    for (int i=0; i<n; ++i)
+        cin>>a[i];
+
+    cout << "please, stop" << endl;
+
+    return 0;
+}
