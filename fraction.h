54f9ef92e0597a281a14c9760082ce6b01d68421
Lina Dva <Anhelina.Pashkowets@gmail.com>
2020-10-29 22:31:29 +0300

    my work:(

---
+++ fraction.h
@@ -0,0 +1,14 @@
+#ifndef LABA6_CLASS_FRACTION_H
+#define LABA6_CLASS_FRACTION_H
+
+
+class fraction {
+private:
+public:
+    int numerator;
+    int denominator;
+    void input_blin(fraction &y);
+};
+
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "fraction.h"
+
+int main() {
+    fraction y;
+    input_blin(y);
+    return 0;
+}
