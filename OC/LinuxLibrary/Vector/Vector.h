c77e9db7f49f4ecda352210a1a546c076d18a365
Lina_Ras <anhelina.pashkowets@gmail.com>
2022-03-06 20:29:34 +0300

    .

---
+++ OC/LinuxLibrary/Vector/Vector.h
@@ -0,0 +1,21 @@
+#ifndef VECTOR_VECTOR_H
+#define VECTOR_VECTOR_H
+
+#include"Number.h"
+
+class Vector{
+public:
+	Number x, y;
+	Vector(Number a, Number b): x(a), y(b){}
+	Vector(): x(ZERO_N), y(ONE_N){}
+
+	Number r();
+	Number angle();
+
+	static Vector add(Vector v1, Vector v2);
+};
+
+const Vector ZERO_V(ZERO_N, ZERO_N);
+const Vector ONE_V(ONE_N, ONE_N);
+
+#endif //VECTOR_VECTOR_H
