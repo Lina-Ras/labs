7aa9b21f46225afc808f49e97b6571c2fdd85f76
Lina_Ras <anhelina.pashkowets@gmail.com>
2022-03-06 17:53:24 +0300

    .

---
+++ OC/LinuxLibrary/Number/Number.cpp
@@ -0,0 +1,47 @@
+#include "Number.h"
+
+#include <cmath>
+#include <iostream>
+
+Number::Number(double val) : value(val) {};
+Number::Number() :value(0) {};
+
+Number Number::add(Number a) {
+    return Number(a.value + this->value);
+}
+Number Number::operator+(Number a) {
+    return Number(a.value + this->value);
+}
+
+Number Number::substract(Number a) {
+    return Number(a.value - this->value);
+}
+Number Number::operator-(Number a) {
+    return Number(this->value - a.value);
+}
+
+Number Number::multiply(Number a) {
+    return Number(a.value * this->value);
+}
+Number Number::operator*(Number a) {
+    return Number(a.value * this->value);
+}
+
+Number Number::divide(Number a) {
+    return Number(this->value / a.value);
+}
+Number Number::operator/(Number a) {
+    return Number(this->value / a.value);
+}
+
+Number Number::sqrtNumber(Number a) {
+    return Number(sqrt(a.value));
+}
+
+Number Number::asinNumber(Number a) { //uses degrees
+    return Number(asin(a.value) * 180 / 3.1416);
+}
+
+Number Number::createNumber(double val) {
+    return Number(val);
+}
