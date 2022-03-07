9647d6c62026fb1d2d387055b7d245a0dfe74000
Lina_Ras <anhelina.pashkowets@gmail.com>
2022-03-08 01:00:15 +0300

    .

---
+++ OC/LinuxLibrary/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Vector/Vector.h"
+
+int main()
+{
+    Number nine(9);
+    Vector one = ONE_V;
+    Vector c(nine, nine);
+
+    std::cout << "c(" <<c.x.value << "," << c.y.value << ")\n";
+
+    std::cout << "r = " << c.r().value << ", angle = " << c.angle().value <<'\n';
+
+    std::cout << "c(" << c.x.value << "," << c.y.value << ") + o(1,1) = ";
+
+    c = Vector::add(c, one);
+
+    std::cout << "c(" << c.x.value << "," << c.y.value << ")\n";
+    return 0;
+}
