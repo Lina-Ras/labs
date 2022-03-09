e8bbb059c62299b31b85f34070a20c1b9db11765
Lina Dva <Anhelina.Pashkowets@gmail.com>
2022-03-09 23:28:57 +0300

    script was added

---
+++ OC/WindowsScript/script.ps1
@@ -0,0 +1,14 @@
+﻿mkdir tmp
+
+g++ -c ./Number/Number.cpp -o ./tmp/Number.o
+ar rvs ./tmp/Number.lib ./tmp/Number.o
+
+g++ -c -LNumber ./Vector/Vector.cpp -o ./tmp/Vector.o
+g++ -shared -o ./tmp/Vector.dll ./tmp/Vector.o ./tmp/Number.lib
+g++ -c -IVector main.cpp -o ./tmp/main.o
+g++ ./tmp/Vector.o ./tmp/Number.o ./tmp/main.o -o main
+
+./main
+
+rm -R tmp
+pause
