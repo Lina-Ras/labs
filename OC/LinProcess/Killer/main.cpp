5a646780f746ff264432f0a19ce753935a6a3949
Lina Dva <Anhelina.Pashkowets@gmail.com>
2022-05-29 21:22:29 +0300

    done!!

--- OC/LinProcess/Killer/main.cpp
+++ OC/LinProcess/Killer/main.cpp
@@ -2,39 +2,38 @@
 #include <string>
 #include <csignal>
 #include <sys/types.h>
+#include <cstdlib>
 
-//bool KillProcessByName(std::string processName){
-//    bool success = false;
-//    PROCESSENTRY32 entry;
-//    entry.dwSize = sizeof(PROCESSENTRY32);
-//
-//    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-//    if (Process32First(snapshot, &entry) == TRUE){
-//        while (Process32Next(snapshot, &entry) == TRUE){
-//            if (entry.szExeFile == processName){
-//                HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, entry.th32ProcessID);
-//                if(TerminateProcess(hProcess, 1)){
-//                    success = true;
-//                }
-//                CloseHandle(hProcess);
-//            }
-//        }
-//    }
-//    CloseHandle(snapshot);
-//    return success;
-//}
-
-bool KillProcessById(pid_t processId){
-    bool success = false;
-    if(kill(processId, SIGTERM) == 0){
-        success = true;
-    } else{
+bool KillProcessById(pid_t pid){
+    if(pid == 0){
         std::cout << "couldn't find process";
+        return false;
     }
+    kill(pid, SIGTERM) == 0;
+    return true;
+}
+
+bool KillProcessByName(std::string processName) {
+    bool success = false;
+    char buf[512];
+    std::string command = "pidof -s " + processName;
+    FILE *cmd_pipe = popen(command.c_str(), "r");
+
+    fgets(buf, 512, cmd_pipe);
+    pid_t pid = strtoul(buf, NULL, 10);
+    if(pid!=0) {
+        command = "killall "+processName;
+        if (!system(command.c_str())){
+            success = true;
+        }
+    }
+    pclose(cmd_pipe);
     return success;
 }
 
-int main(int argc, char* argv[]) {
+int main() {
+    std::cout << "==================================================================\n";
+    std::cout << "Welcome to Killer.exe!!\n";
     std::cout << "Would you...\n";
     int answ;
     std::cout << "1 -- kill process by name\n";
@@ -49,12 +48,11 @@ int main(int argc, char* argv[]) {
                 std::cout << "\nEnter process name: ";
                 std::string procName;
                 std::cin >> procName;
-
-//                if(KillProcessByName(procName)){
-//                    std::cout << "Success!\n";
-//                } else{
-//                    std::cout << ":(\n";
-//                }
+                if(KillProcessByName(procName)){
+                    std::cout << "Success!\n";
+                } else{
+                    std::cout << ":(\n";
+                }
                 break;
             }
             case 2:{
@@ -75,13 +73,15 @@ int main(int argc, char* argv[]) {
                 size_t pos = 0;
                 std::string token;
                 while ((pos = proc_to_kill.find(',')) != std::string::npos) {
-                    KillProcessByName(proc_to_kill.substr(0, pos)+".exe");
+                    KillProcessByName(proc_to_kill.substr(0, pos));
                     proc_to_kill.erase(0, pos + 1);
                 }
-                KillProcessByName(proc_to_kill.substr(0, pos)+".exe");
+                KillProcessByName(proc_to_kill.substr(0, pos));
                 break;
             }
             case 4:{
+                std::cout << "bye! :)\n";
+                std::cout << "==================================================================\n";
                 return 0;
             }
         }
--- OC/LinProcess/main.cpp
+++ OC/LinProcess/main.cpp
@@ -1,78 +1,49 @@
 #include <string>
 #include <iostream>
-#include <windows.h>
-#include <Process.h>
-#include <psapi.h>
 #include <vector>
+#include <cstdlib>
 
-void ShowActiveProcesses(){
-    std::vector<std::string> processes;
-    DWORD aProcesses[1024], cbNeeded, cProcesses;
-    unsigned int i;
+void ShowActiveProcesses(std::vector<std::string>& vec){
+    std::cout << "\nPROCESSES STATISTIC: \n";
+    bool flag = false;
+    for(auto& el: vec){
+        char buf[512];
+        std::string command = "pidof -s " + el;
+        FILE *cmd_pipe = popen(command.c_str(), "r");
 
-    if ( !EnumProcesses( aProcesses, sizeof(aProcesses), &cbNeeded ) )
-    {
-        return;
-    }
-    cProcesses = cbNeeded / sizeof(DWORD);
-    for ( i = 0; i < cProcesses; i++ ){
-        if( aProcesses[i] != 0 ){
-            TCHAR szProcessName[MAX_PATH] = TEXT("<unknown>");
-            HANDLE hProcess = OpenProcess( PROCESS_QUERY_INFORMATION |
-                                           PROCESS_VM_READ,
-                                           FALSE, aProcesses[i] );
-            if (NULL != hProcess ){
-                HMODULE hMod;
-                DWORD cbNeeded;
-
-                if ( EnumProcessModules( hProcess, &hMod, sizeof(hMod), &cbNeeded) ){
-                    GetModuleBaseName(hProcess, hMod, szProcessName, sizeof(szProcessName)/sizeof(TCHAR) );
-                }
-            }
-            processes.push_back(szProcessName);
-            CloseHandle( hProcess );
+        fgets(buf, 512, cmd_pipe);
+        pid_t pid = strtoul(buf, NULL, 10);
+        if(pid != 0 ) {
+            flag = true;
+            std::cout << el << " is running" << '\n';
         }
+        pclose(cmd_pipe);
     }
-
-    bool telegram_is_active = false;
-    bool notepad_is_active = false;
-    for (auto x : processes) {
-        if (x == "Telegram.exe") {
-            std::cout << "\nTelegram is active!";
-            telegram_is_active = true;
-        }
-        if (x == "notepad.exe") {
-            std::cout << "\nNotepad is active!";
-            notepad_is_active = true;
-        }
-    }
-    if(!notepad_is_active && !telegram_is_active){
-        std::cout << "\nNone of the processes we are intressted in are running";
+    if(!flag){
+        std::cout << "No processes from PROC_TO_KILL are running\n";
     }
 }
 
-int main(int argc,char **argv) {
-    _putenv_s("PROC_TO_KILL", "notepad,Telegram");
-    ShowActiveProcesses();
-    std::string pf = getenv("PROC_TO_KILL");
+int main() {
+    setenv("PROC_TO_KILL", "telegram-desktop,firefox", 0);
+    std::string proc_to_kill = getenv("PROC_TO_KILL");
 
-    char lpszAppName[] = R"(..\Killer\cmake-build-debug\Killer.exe)"; //open Killer exe
-    STARTUPINFO si;
-    PROCESS_INFORMATION pi;
-    ZeroMemory(&si, sizeof(STARTUPINFO));
-    si.cb = sizeof(STARTUPINFO);
-    if (CreateProcess(lpszAppName, NULL, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL,
-                       NULL, &si, &pi)){
-        Sleep(1000);
-        WaitForSingleObject(pi.hProcess, INFINITE);
-        CloseHandle(pi.hThread);
-        CloseHandle(pi.hProcess);
-    } else{
+    std::vector<std::string> processes;
+    size_t pos = 0;
+    std::string token;
+    while ((pos = proc_to_kill.find(',')) != std::string::npos) {
+        processes.push_back(proc_to_kill.substr(0, pos));
+        proc_to_kill.erase(0, pos + 1);
+    }
+    processes.push_back(proc_to_kill.substr(0, pos));
+
+    ShowActiveProcesses(processes);
+
+    if (system(R"(../Killer/cmake-build-debug/Killer)")){
         std::cout << "The meow process is not created.\n";
         return 0;
     }
-
-    ShowActiveProcesses();
-    _putenv("PROC_TO_KILL"); //delete
+    ShowActiveProcesses(processes);
+    clearenv();//delete
     return 0;
 }
